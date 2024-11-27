c814d9f990f9cb1859fa9978224740c76190ba00
ayushathapa16 <thapaayusha16@gmail.com>
2024-11-27 18:09:27 -0500

    decluttered all folders

--- src/main/java/entity/CommonGuestFactory.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-public class CommonGuestFactory implements GuestFactory{
-
-    @Override
-    public Guest create(String username, String email, String phone, String arrival, String departure) {
-        return new CommonGuest(username, email, phone, arrival, departure);
-    }
-}
--- src/main/java/entity/GuestFactory.java
+++
@@ -1,6 +0,0 @@
-package Entity;
-
-public interface GuestFactory
-{
-    Guest create(String username, String email, String phone, String arrival, String departure);
-}
