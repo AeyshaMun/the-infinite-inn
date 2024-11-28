ac1d08967945b94b1b73371e619b884d3175e977
Aeysha Munawwarah <aeysha.munawwarah@gmail.com>
2024-11-27 21:10:30 -0500

    Add PersonFactory interface for creating Person objects - Introduced PersonFactory interface with a create method to abstract the creation of Person objects. - The create method takes a name parameter and returns a new Person instance.

---
+++ src/main/java/entity/PersonFactory.java
@@ -0,0 +1,14 @@
+package main.java.entity;
+
+/**
+ * Factory for creating users.
+ */
+public interface PersonFactory {
+    /**
+     * Creates a new Person.
+     * @param name the name of the new person
+     * @return the new person
+     */
+    Person create(String name);
+
+}
