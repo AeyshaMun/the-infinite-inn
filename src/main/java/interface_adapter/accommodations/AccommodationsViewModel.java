163f378c511a631349572ce8d2391448554fc6f8
Olivia <oliviajq.zhou@mail.utoronto.ca>
2024-12-02 13:11:43 -0500

    DataAccessInterface, Interactor, OutputBoundary, State, View, ViewModel, UpdatedDAO, updated pom file to include mockito

--- src/main/java/interface_adapter/accommodations/AccommodationsViewModel.java
+++ src/main/java/interface_adapter/accommodations/AccommodationsViewModel.java
@@ -1,4 +1,47 @@
 package interface_adapter.accommodations;
 
-public class AccommodationsViewModel {
-}
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+    public class AccommodationsViewModel {
+        private PropertyChangeSupport support;
+        private String name;
+        private String accommodations;
+
+        public AccommodationsViewModel() {
+            support = new PropertyChangeSupport(this);
+        }
+
+        public void firePropertyChanged(String accommodations) {
+            support.firePropertyChange(accommodations, null, accommodations);
+        }
+
+        public void addPropertyChangeListener(PropertyChangeListener pcl) {
+            support.addPropertyChangeListener(pcl);
+        }
+
+        public void removePropertyChangeListener(PropertyChangeListener pcl) {
+            support.removePropertyChangeListener(pcl);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            String oldName = this.name;
+            this.name = name;
+            support.firePropertyChange("name", oldName, name);
+        }
+
+        public String getAccommodations() {
+            return accommodations;
+        }
+
+        public void setAccommodations(String accommodations) {
+            String oldAccommodations = this.accommodations;
+            this.accommodations = accommodations;
+            support.firePropertyChange("accommodationsUpdate", oldAccommodations, accommodations);
+        }
+    }
+
