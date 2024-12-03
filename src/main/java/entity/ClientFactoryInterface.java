76f409f7078bca2faf4d32b029573c1c39486e83
Aeysha Munawwarah <aeysha.munawwarah@gmail.com>
2024-12-03 10:55:03 -0500

    Fixed the interface so it extends PersonFactory not RoomFactory

--- src/main/java/entity/ClientFactoryInterface.java
+++ src/main/java/entity/ClientFactoryInterface.java
@@ -2,7 +2,7 @@ package entity;
 
 import java.util.ArrayList;
 
-public interface ClientFactoryInterface extends RoomFactory {
+public interface ClientFactoryInterface extends PersonFactory {
     Person createClient(String name, String accommodations, ArrayList<Integer> order, EventBooking eventBooking);
 
 }
